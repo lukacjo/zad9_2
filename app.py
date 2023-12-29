b39a757e30d18c2cc176cd533d5da742e9a35e09
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-12-29 21:48:59 +0100

    visual changes

--- app.py
+++ app.py
@@ -1,35 +1,46 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 import requests
 import csv
 
 app = Flask(__name__)
+app.config[
+    "SECRET_KEY"
+] = "eh2WGSwY@HwO$!S!j32EM*9$36zKJCyvrvu#fRVV3rgs$3@H&whPGXwknMyW#qypW%E#D8yhTPDq$m##Ho6wUkNdIeuWozI8dZM"
 
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
-rates = data[0].get("rates")
-with open("data_file.csv", "w", encoding="UTF8", newline="") as f:
-    writer = csv.DictWriter(
-        f, fieldnames=["code", "currency", "ask", "bid"], delimiter=";"
-    )
-    writer.writeheader()
-    writer.writerows(rates)
+
+def zdobycie_i_zapisanie_walut():
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data = response.json()
+    rates = data[0].get("rates")
+    with open("data_file.csv", "w", encoding="UTF8", newline="") as f:
+        writer = csv.DictWriter(
+            f, fieldnames=["code", "currency", "ask", "bid"], delimiter=";"
+        )
+        writer.writeheader()
+        writer.writerows(rates)
+    return rates
 
 
 @app.route("/nbp/", methods=["GET", "POST"])
 def nbp():
+    rates = zdobycie_i_zapisanie_walut()
     codes = []
-    result = []
+
     for data in rates:
         codes.append(data.get("code"))
     if request.method == "POST":
         code = request.form.get("currency")
-        ile = request.form.get("howmuch")
+        ile = int(request.form.get("howmuch"))
+
         for data in rates:
             if data.get("code") == code:
                 ask = data.get("ask")
-        ile = int(ile)
+        ile = ile
         elo = ask * ile
-        return f"Koszt operacji to:{elo}zł ", result
+        flash(
+            f"Koszt operacji w przypadku waluty {code} to: {elo:.2f}zł",
+            category="success",
+        )
     return render_template("zad9.2.html", codes=codes, rates=rates)
 
 
