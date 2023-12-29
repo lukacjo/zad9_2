40e1a49f39c9361b766e18f90c47f8cd3719f7d8
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-12-29 11:56:40 +0100

    visual changes

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
-rates = data[0].get('rates')
-with open('data_file.csv', 'w', encoding='UTF8', newline='') as f :
-    writer = csv.DictWriter(f, fieldnames=['code', 'currency', 'ask', 'bid'],delimiter=';')
+rates = data[0].get("rates")
+with open("data_file.csv", "w", encoding="UTF8", newline="") as f:
+    writer = csv.DictWriter(
+        f, fieldnames=["code", "currency", "ask", "bid"], delimiter=";"
+    )
     writer.writeheader()
     writer.writerows(rates)
 
@@ -18,36 +20,18 @@ def nbp():
     codes = []
     result = []
     for data in rates:
-        codes.append(data.get('code'))
+        codes.append(data.get("code"))
     if request.method == "POST":
-        code=request.form.get("currency")
-        ile=request.form.get("howmuch")
+        code = request.form.get("currency")
+        ile = request.form.get("howmuch")
         for data in rates:
-            if data.get('code') == code:
-                ask = data.get('ask')
-        ile=int(ile)
-        elo=ask*ile
+            if data.get("code") == code:
+                ask = data.get("ask")
+        ile = int(ile)
+        elo = ask * ile
         return f"Koszt operacji to:{elo}zł ", result
-    return render_template("zad9.2.html",codes=codes,rates=rates)
+    return render_template("zad9.2.html", codes=codes, rates=rates)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
