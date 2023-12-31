ab2ce849b025dd45fcac2d916384796dec393632
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-12-31 16:02:37 +0100

    przyspieszanie funkcji

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request, redirect, flash
 import requests
 import csv
+import time
+
+"""
+Najlepsze czasy
+Wersja pierwsza:
+zdobycie: 0.0016s
+nbp 0.052s
+
+Wersja druga (z set):
+zdobycie: 0.0005s
+nbp: 0.040s
+"""
 
 
 def create_app():
@@ -14,40 +26,48 @@ def create_app():
 app = create_app()
 
 
-def zdobycie_i_zapisanie_walut():
+def fetch_and_save_currencies():
     response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
     data = response.json()
-    rates = data[0].get("rates")
+    start_time = time.time()
+    rates = {currency["code"]: currency for currency in data[0].get("rates")}
     with open("data_file.csv", "w", encoding="UTF8", newline="") as f:
         writer = csv.DictWriter(
             f, fieldnames=["code", "currency", "ask", "bid"], delimiter=";"
         )
         writer.writeheader()
-        writer.writerows(rates)
+        writer.writerows(rates.values())
+    end_time = time.time()
+    real_time = end_time - start_time
+    print(f"Czas fetch_and_save to: {real_time}s")
     return rates
+
+
+fetch_and_save_currencies()
 
 
 @app.route("/nbp/", methods=["GET", "POST"])
 def nbp():
-    rates = zdobycie_i_zapisanie_walut()
-    codes = []
+    start_time = time.time()
+    rates = fetch_and_save_currencies()
+    codes = set(rates.keys())
 
-    for data in rates:
-        codes.append(data.get("code"))
     if request.method == "POST":
         code = request.form.get("currency")
         ile = int(request.form.get("howmuch"))
 
-        for data in rates:
-            if data.get("code") == code:
-                ask = data.get("ask")
-        ile = ile
+        currency_info = rates[code]
+        ask = currency_info["ask"]
         elo = ask * ile
         flash(
             f"Koszt zakupu {ile} {code} to: {elo:.2f}zł",
             category="success",
         )
-    return render_template("zad9.2.html", codes=codes, rates=rates)
+        end_time = time.time()
+        real_time = end_time - start_time
+        print(f"Czas nbp to: {real_time}s")
+
+    return render_template("zad9.2.html", codes=codes, rates=rates.values())
 
 
 if __name__ == "__main__":
